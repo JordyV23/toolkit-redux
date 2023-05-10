0244ddd5fdc5a5a32e67954b43e101fb71e99e64
JordyV23 <jordyvargas0123@gmail.com>
2023-05-09 19:55:05 -0600

    Creacion de slices para pokemon api

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Proyecto Toolkit redux
 
 Este proyecto tiene el fin de dar los primeros pasos en redux.
+
+# Thunks: 
+Es una accion asincrona que dispara otra accion cuando se resuleve la peticion asincrona.
---
+++ src/PokemonApp.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { getPokemons } from "./store/slices/pokemon/thunks"
+
+export const PokemonApp = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    dispatch(getPokemons());
+  },[])
+
+  return (
+    <>
+        <h1>PokemonApp</h1>
+        <hr />
+
+        <ul>
+            <li>Test</li>
+        </ul>
+    </>
+  )
+}
